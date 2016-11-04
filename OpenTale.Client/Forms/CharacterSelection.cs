8e6de39c07e54b8b8d2de3d36cc8536a44b5c8d6
Ciapa <ciapa1998@gmail.com>
2016-11-04 13:05:54 +0100

    Setup basic project structure

---
+++ OpenTale.Client/Forms/CharacterSelection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenTale.Client.Forms
+{
+    public partial class CharacterSelection : Form
+    {
+        public CharacterSelection()
+        {
+            InitializeComponent();
+        }
+    }
+}
